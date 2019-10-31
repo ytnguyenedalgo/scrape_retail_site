de1b97a712cfd22c371826d7ccd7095ac461a13e
Ye N.E <ytnguyenedalgo@gmail.com>
2019-10-31 12:46:31 -0400

    Optimization

--- ScrapeRetailSite.py
+++ ScrapeRetailSite.py
@@ -23,12 +23,12 @@ from bs4 import BeautifulSoup as Soup
 import requests
 import csv
 import apikey
-import os
 
         
 class Scraper: 
     def __init__(self, site="https://www.macys.com"):
         self.site = site 
+        self.my_api_key = str(apikey.Key())
         self.Categories = set()
         self.URLs = set()
         self.products = []
@@ -37,20 +37,22 @@ class Scraper:
 
     def get_parse(self, site): 
         agent = {"User-Agent":"Mozilla/5.0"} 
-        my_api_key = str(apikey.Key())
-        payload = {'api_key':my_api_key, 'url':site}
-        #page = requests.get(site, headers=agent)
+        page = requests.get(site, headers=agent)
+        #print(page.status_code)
+        
         #For when using Scraper API to avoid getting blocked 
-        page = requests.get('http://api.scraperapi.com', 
-                            headers = agent,
-                            params = payload, timeout=60) 
+# =============================================================================
+#         payload = {'api_key':self.my_api_key, 'url':site}
+#         page = requests.get('http://api.scraperapi.com', 
+#                             headers = agent,
+#                             params = payload, timeout=60) 
+# =============================================================================
         self.soup = Soup(page.content, "html.parser")
     
 
     def get_url_categories(self):
         self.get_parse(self.site)
         soup = self.soup
-        #print(soup)
         for tag in soup.find_all("a", href=True):
             path = tag["href"]
             if "http" not in path\
@@ -63,10 +65,18 @@ class Scraper:
     
     def get_url_products(self):
         self.get_url_categories()
+        
+        #For small sample testing
+# =============================================================================
 #         test = self.Categories.pop()
 #         self.Categories.add(test)
 #         self.get_parse(test)
 #         soup = self.soup
+#         for tag in soup.find_all("a", {"class": "productDescLink"}):
+#                 path = tag.get("href")
+#                 self.URLs.add(self.site+path)
+# =============================================================================
+                
         for url in self.Categories:
             self.get_parse(url)
             soup = self.soup
@@ -77,33 +87,28 @@ class Scraper:
         return self.URLs
             
         
-    def scrape(self):
+    def scrape_and_save(self):
         self.get_url_products()
-        for url in self.URLs:
-            self.get_parse(url)
-            soup = self.soup
-            try: 
-                name = (((soup.find_all("h1", {"class": "p-name h3"})[0].text)\
-                        .replace("\n","")).strip()).upper()
-                price = ((soup.find_all("div", {"class": "price"})[0].text)\
-                         .replace("\n","")).strip()
-                des = ((soup.find_all("p", {"data-auto": "product-description"})[0].text)\
-                       .replace("\n","")).strip()
-                self.products.append([name, price, des])
-            except IndexError:
-                pass
+        with open("macys-products.cvs", "w+") as csvf:
+            w = csv.writer(csvf, delimiter=",")
+            for url in self.URLs:
+                self.get_parse(url)
+                soup = self.soup
+                try: 
+                    name = (((soup.find_all("h1", {"class": "p-name h3"})[0].text)\
+                            .replace("\n","")).strip()).upper()
+                    price = ((soup.find_all("div", {"class": "price"})[0].text)\
+                             .replace("\n","")).strip()
+                    des = ((soup.find_all("p", {"data-auto": "product-description"})[0].text)\
+                           .replace("\n","")).strip()
+                    self.products.append([name, price, des])
+                    w.writerow(self.products[-1])
+                except IndexError:
+                    pass
             
         return self.products
     
-    
-    def save_cvs(self):
-        self.scrape()
-        with open("macys-products.cvs", "w+") as csvf:
-            w = csv.writer(csvf, delimiter=",")
-            for i in self.products:
-                w.writerow(i)
-                
-    
+        
     def get_product_info(self, name):
         name = str(name).upper()
         with open("macys-products.cvs", "r") as csvf:
@@ -119,10 +124,8 @@ class Scraper:
 scrape = Scraper()
 
 #=================Scrape and save data from macys.com=============
-#scrape.scrape()
-scrape.save_cvs()
+scrape.scrape_and_save()
 
 #==============Search product info by name========================
 product_name = input("Search product name: ")
 scrape.get_product_info(product_name)
-
--- apikey.py
+++ apikey.py
@@ -1,6 +1,7 @@
 class Key():
     def __init__(self):
-        self.key= input("Type in your API key provided by scraperapi.com: ")
+        self.key= input("Type in your API key provided by scraperapi.com\
+			\nIf you don't have one, Enter any key to continue: ")
 
 
     def __repr__(self):
